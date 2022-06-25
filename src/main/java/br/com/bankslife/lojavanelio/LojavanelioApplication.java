5a4eb36b4146b1cb854377548fce5c3f2ce76490
felipecabesssa <felipesoares100@hotmail.com>
2022-06-24 22:01:47 -0300

    Projeto criado

---
+++ src/main/java/br/com/bankslife/lojavanelio/LojavanelioApplication.java
@@ -0,0 +1,13 @@
+package br.com.bankslife.lojavanelio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojavanelioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojavanelioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bankslife/lojavanelio/LojavanelioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bankslife.lojavanelio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojavanelioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
